ca01c98647ca2c0bf01a9d9e9d0716e100fdcfc0
Ricard Penin Honrubia <ergoegos@gmail.com>
2025-02-12 21:24:36 +0100

    asdsd

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,4 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
-$routes->get('/auth', 'Auth::index');
+// $routes->get('/auth', 'Auth::index');
+$routes->get('login', 'Auth::login');
+$routes->post('auth/process', 'Auth::process');
+$routes->get('auth/logout', 'Auth::logout');
+$routes->get('dashboard', 'Dashboard::index');
---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Dashboard extends Controller
+{
+    public function index()
+    {
+        if (!session()->get('logged_in')) {
+            return redirect()->to('/login');
+        }
+        
+        return view('dashboard');
+    }
+}
